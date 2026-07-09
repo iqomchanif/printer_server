8db862a92c18cf13f053cb134485b63b738a9618
iqomchanif <chanif.qomaruddin@gmail.com>
2026-07-09 21:15:57 +0700

    ok

--- test.js
+++ test.js
@@ -1,15 +1,25 @@
-const usb = require('usb');
-console.log(Object.keys(usb));
+const { usb } = require('usb');
 
 const dev = usb.findByIds(0x04b8, 0x0046);
+
+console.log(dev);
+
 dev.open();
 
 const iface = dev.interfaces[0];
+
+// kalau error "kernel driver active", nanti perlu detach
+if (iface.isKernelDriverActive()) {
+  iface.detachKernelDriver();
+}
+
 iface.claim();
 
 const epIn = iface.endpoints.find(e => e.direction === 'in');
 
+console.log(epIn);
+
 epIn.transfer(64, (err, data) => {
-    console.log(err);
-    console.log(data);
+  console.log('err:', err);
+  console.log('data:', data);
 });
