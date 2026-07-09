77c4b40287a242aadcf79e98b4bb8fb454fac100
iqomchanif <chanif.qomaruddin@gmail.com>
2026-07-09 21:08:42 +0700

    ok

---
+++ test.js
@@ -0,0 +1,14 @@
+const usb = require('usb');
+
+const dev = usb.findByIds(0x04b8, 0x0046);
+dev.open();
+
+const iface = dev.interfaces[0];
+iface.claim();
+
+const epIn = iface.endpoints.find(e => e.direction === 'in');
+
+epIn.transfer(64, (err, data) => {
+    console.log(err);
+    console.log(data);
+});
