522bb593420f9360703960bd9e587e4fe2ec348c
iqomchanif <chanif.qomaruddin@gmail.com>
2026-07-09 21:18:09 +0700

    ok

--- test.js
+++ test.js
@@ -3,25 +3,27 @@ const { usb } = require('usb');
     const dev = await usb.findDeviceByIds(0x04b8, 0x0046);
 
     console.log(dev);
+
+
+    console.log(dev);
+
+    dev.open();
+
+    const iface = dev.interfaces[0];
+
+    // kalau error "kernel driver active", nanti perlu detach
+    if (iface.isKernelDriverActive()) {
+    iface.detachKernelDriver();
+    }
+
+    iface.claim();
+
+    const epIn = iface.endpoints.find(e => e.direction === 'in');
+
+    console.log(epIn);
+
+    epIn.transfer(64, (err, data) => {
+    console.log('err:', err);
+    console.log('data:', data);
+    });
 })();
-console.log(dev);
-
-dev.open();
-
-const iface = dev.interfaces[0];
-
-// kalau error "kernel driver active", nanti perlu detach
-if (iface.isKernelDriverActive()) {
-  iface.detachKernelDriver();
-}
-
-iface.claim();
-
-const epIn = iface.endpoints.find(e => e.direction === 'in');
-
-console.log(epIn);
-
-epIn.transfer(64, (err, data) => {
-  console.log('err:', err);
-  console.log('data:', data);
-});
